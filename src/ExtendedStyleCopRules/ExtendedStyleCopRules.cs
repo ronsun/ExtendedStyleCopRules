532646dbdde6918e6f8183e4dc26f5ca5ed1e502
ron <ron.sun@hotmail.com.tw>
2018-03-30 18:02:17 +0800

    add rule: PrivateFieldNamesMustStartWithUnderscore

--- src/ExtendedStyleCopRules/ExtendedStyleCopRules.cs
+++ src/ExtendedStyleCopRules/ExtendedStyleCopRules.cs
@@ -11,5 +11,30 @@ namespace ExtendedStyleCopRules
     [SourceAnalyzer(typeof(CsParser))]
     public class ExtendedStyleCopRules : SourceAnalyzer
     {
+        public override void AnalyzeDocument(CodeDocument document)
+        {
+            CsDocument csdocument = (CsDocument)document;
+
+            if (csdocument.RootElement != null && !csdocument.RootElement.Generated)
+            {
+                CheckNamingRule(csdocument.RootElement);
+            }
+        }
+
+        private void CheckNamingRule(CsElement element)
+        {
+            if (element.ElementType == ElementType.Field && element.AccessModifier == AccessModifierType.Private)
+            {
+                if (!element.Declaration.Name.StartsWith("_"))
+                {
+                    AddViolation(element, Rules.PrivateFieldNamesMustStartWithUnderscore);
+                }
+            }
+
+            foreach (var child in element.ChildElements)
+            {
+                CheckNamingRule(child);
+            }
+        }
     }
 }
