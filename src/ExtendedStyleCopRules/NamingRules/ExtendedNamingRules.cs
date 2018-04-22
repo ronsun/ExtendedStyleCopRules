70bbace363e6d098dd19d013d46c4d5fbc2c5410
ron.sun <ron.sun@hotmail.com.tw>
2018-04-22 17:40:56 +0800

    implement ExtendedNamingRules.cs by walker

--- src/ExtendedStyleCopRules/NamingRules/ExtendedNamingRules.cs
+++ src/ExtendedStyleCopRules/NamingRules/ExtendedNamingRules.cs
@@ -11,37 +11,55 @@ namespace ExtendedStyleCopRules.NamingRules
         {
             CsDocument csdocument = (CsDocument)document;
 
-            if (csdocument.RootElement != null && !csdocument.RootElement.Generated)
-            {
-                CheckNamingRule(csdocument.RootElement);
-            }
+            csdocument.WalkDocument(new CodeWalkerElementVisitor<object>(VisitElement));
         }
 
-        private void CheckNamingRule(CsElement element)
+        private bool VisitElement(CsElement element, CsElement parentElement, object context)
         {
-            if (element.ElementType == ElementType.Field && element.AccessModifier == AccessModifierType.Private)
+            // not target element
+            if (element.Generated ||
+                element.ElementType != ElementType.Field ||
+                element.AccessModifier != AccessModifierType.Private)
             {
-                if (!element.Declaration.Name.StartsWith("_"))
-                {
-                    AddViolation(element, Rules.PrivateFieldNamesMustStartWithUnderscore);
-                }
+                return true;
             }
 
-            var typesContainFiled = new List<ElementType>()
+            if (!element.Declaration.Name.StartsWith("_"))
             {
-                ElementType.Class,
-                ElementType.Namespace,
-                ElementType.Root,
-                ElementType.Struct
-            };
-
-            if (typesContainFiled.Contains(element.ElementType))
-            {
-                foreach (var child in element.ChildElements)
-                {
-                    CheckNamingRule(child);
-                }
+                AddViolation(element, Rules.PrivateFieldNamesMustStartWithUnderscore);
             }
+
+            return true;
         }
+
+        #region Sample for another way to write customer rules
+
+        //public override void AnalyzeDocument(CodeDocument document)
+        //{
+        //    CsDocument csdocument = (CsDocument)document;
+
+        //    if (csdocument.RootElement != null && !csdocument.RootElement.Generated)
+        //    {
+        //        CheckNamingRule(csdocument.RootElement);
+        //    }
+        //}
+
+        //private void CheckNamingRule(CsElement element)
+        //{
+        //    if (element.ElementType == ElementType.Field && element.AccessModifier == AccessModifierType.Private)
+        //    {
+        //        if (!element.Declaration.Name.StartsWith("_"))
+        //        {
+        //            AddViolation(element, Rules.PrivateFieldNamesMustStartWithUnderscore);
+        //        }
+        //    }
+
+        //    foreach (var child in element.ChildElements)
+        //    {
+        //        CheckNamingRule(child);
+        //    }
+        //}
+
+        #endregion
     }
 }
